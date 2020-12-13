fce34ef23a5cc8672643478f00049eee40a368a2
DynamoVue <truongvuetify@gmail.com>
2020-12-13 15:06:48 +0700

    Prepare for deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'my-app', 'build')));
 
-  app.get('/', function (req, res) {
+  app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, 'client', 'my-app', 'build', 'index.html'));
   });
 }
